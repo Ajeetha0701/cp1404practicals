ed6b0885092dc8c4adbd21905767e34bab1d8f1b
Ajeetha <feross.ajeetha@my.jcu.edu.au>
2024-03-22 17:20:41 +0800

    Add practical 6

---
+++ prac_06/guitars.py
@@ -0,0 +1,31 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}), worth ${self.cost:,.2f}"
+
+    def is_vintage(self):
+        return self.year < 1922
+
+
+def main():
+    print("My guitars!")
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name (leave blank to stop): ")
+
+    print("\nThese are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:<20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+if __name__ == '__main__':
+    main()
